ba7ef3e1e0a9e5f4a7fef0774b0c7f9162e85af8
Alexander0909M <ElDiablo66666@ya.ru>
2026-09-16 14:11:34 +0500

    Version 1.3

--- src/main/java/ru/yandex/practicum/sleeptracker/LongestSleepSession.java
+++ src/main/java/ru/yandex/practicum/sleeptracker/LongestSleepSession.java
@@ -2,7 +2,7 @@ import java.time.Duration;
 import java.util.List;
 import java.util.function.Function;
 
-public class LongestSleepSession implements Function<List<SleepingSession>, Duration>{
+public class LongestSleepSession implements Function<List<SleepingSession>, Duration> {
     @Override
     public Duration apply(List<SleepingSession> sleepingSessions) {
         if (sleepingSessions == null || sleepingSessions.isEmpty()) return Duration.ZERO;
